38912a1dbebc618ba7aa37b58a08b47ce98a3eef
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2025-02-06 16:10:55 +0100

    adding Stuff

--- Cerberus_Pass/PasswordManager.cs
+++ Cerberus_Pass/PasswordManager.cs
@@ -8,6 +8,37 @@ namespace Cerberus_Pass
 {
     internal class PasswordManager
     {
+        private List<PasswordEntry> vault;
 
+        public PasswordManager()
+        {
+            // vault = new List<PasswordEntry>();
+            // vault = new();
+            // vault = [];
+            vault = [];
+        }
+        //public List<PasswordEntry> GetAll()
+        //{
+        //    return vault;
+        //}
+        public List<PasswordEntry> GetAll() => vault;
+       
+        public PasswordEntry CreateEntry(
+            string title,
+            string login,
+            string password,
+            string website = "",
+            string note = "")
+        {
+            PasswordEntry newEntry = new PasswordEntry(
+                title,
+                login,
+                password,
+                website,
+                note);
+            vault.Add(newEntry);
+            return newEntry;
+        }
+       
     }
 }
--- Cerberus_Pass/Program.cs
+++ Cerberus_Pass/Program.cs
@@ -10,24 +10,71 @@
 
             PasswordEntry? pass1 = new PasswordEntry(title: "Steam", login: "WaldmeisterSD", password: "P@ssword");
             PasswordEntry? pass2 = new PasswordEntry(title: "Steam", login: "WaldmeisterSD", password: "P@ssword", website: "https://store.steampowered.com", note: "Mein cooler Steam-Acount");
-
             Console.WriteLine(value: pass1.Website.Length);
 
+            var manager = new PasswordManager();
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine(value: "\t\tWillkommen im Cerberus-Pass!");
             Console.ResetColor();
 
-            Console.ForegroundColor= ConsoleColor.Yellow;
-            Console.WriteLine(value: "\nWähle was Du tun willst: ");
+            do
+            {
 
-            Console.WriteLine(value: """
-                1. Passwort-Liste ausgeben
-                2. Passwort mit ID ausgeben
-                3. Neues Passwort erstellen
-                4. Vorhandenes Passwort bearbeiten
-                5. Passwort löschen
-                """);
-            Console.ResetColor();
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.WriteLine(value: "\nWähle was Du tun willst: ");
+
+                Console.WriteLine(value: """
+                    1. Passwort-Liste ausgeben
+                    2. Passwort mit ID ausgeben
+                    3. Neues Passwort erstellen
+                    4. Vorhandenes Passwort bearbeiten
+                    5. Passwort löschen
+                    """);
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        // Liste ausgeben
+                        var vault = manager.GetAll();
+                        Console.WriteLine(value: vault);
+                        break;
+                    case "2":
+                        // Passwort anhand ID ausgeben
+                        break;
+                    case "3":
+                        // Erstellen
+                        Console.WriteLine("Gebe einen Titel für den Eintrag an: ");
+                        string title = Console.ReadLine();
+                        Console.WriteLine("Gebe einen Login für den Eintrag an: ");
+                        string login = Console.ReadLine();
+                        Console.WriteLine("Gebe einen Passwort für den Eintrag an: ");
+                        string password = Console.ReadLine();
+                        var newEntry = manager.CreateEntry(title, login, password);
+                        Console.WriteLine(value: "Neuer Eintrag erfolgreich erstellt");
+                        Console.WriteLine(value: newEntry);     // Gibt Type aus;
+                        break;
+                    case "4":
+                        // Update
+                        break;
+                    case "5":
+                        // Delete
+                        break;
+                    default:
+                        // Fehler anzeigen -> Eingabe-Hint (1-5)
+                        // Eingabe wiederholen
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+
+            } while (true);
+
+
+
+
+
 
         }
     }
