821eb089e1c053e60b27e61a2ad3fe4c6c1cdc76
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2025-02-06 14:47:24 +0100

    adding class Entries n PasswordManager

---
+++ Cerberus_Pass/Entries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerberus_Pass
+{   
+    // PascalCase => BegriffstrennungImmerErsterBuchstabeGroß
+    // => Properies, Funkionen / Methods, Classes, Namespaces, Enums
+    internal class PasswordEntry
+    {
+        public string Title { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Website { get; set; }
+        public string Note { get; set; }
+
+        public PasswordEntry(string title, string login, string password, string website = "", string note = "")
+        {
+            Title = title;
+            Login = login;
+            Password = password;
+            Website = website;
+            Note = note;
+        }
+    }
+}
--- Cerberus_Pass/Program.cs
+++ Cerberus_Pass/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Main UI-Flow
+            //using Cerberus_Pass;
+
+            PasswordEntry? pass1 = new PasswordEntry(title: "Steam", login: "WaldmeisterSD", password: "P@ssword");
+            PasswordEntry? pass2 = new PasswordEntry(title: "Steam", login: "WaldmeisterSD", password: "P@ssword", website: "https://store.steampowered.com", note: "Mein cooler Steam-Acount");
+
+            Console.WriteLine(value: pass1.Website.Length);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(value: "\t\tWillkommen im Cerberus-Pass!");
+            Console.ResetColor();
+
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine(value: "\nWähle was Du tun willst: ");
+
+            Console.WriteLine(value: """
+                1. Passwort-Liste ausgeben
+                2. Passwort mit ID ausgeben
+                3. Neues Passwort erstellen
+                4. Vorhandenes Passwort bearbeiten
+                5. Passwort löschen
+                """);
+            Console.ResetColor();
+
         }
     }
 }
