c4b0094036cd22c9e6523688235555acd41b3309
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2025-02-06 12:51:45 +0100

    first commit

---
+++ Cerberus_Pass/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cerberus_Pass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
